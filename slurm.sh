25952bc7e5eb6f3dfbbe65323a1df7ba7d7a8bd1
James Chuang <chuang.ja@gmail.com>
2018-12-04 18:47:22 -0500

    I've been requesting cores incorrectly this whole time...

--- slurm.sh
+++ slurm.sh
@@ -3,9 +3,9 @@
 #SBATCH -p medium
 #SBATCH -t 24:00:00
 #SBATCH --mem-per-cpu=16G
-#SBATCH -n 1
+#SBATCH -c 1
 #SBATCH -e snakemake.err
 #SBATCH -o snakemake.log
 #SBATCH -J MNase-snakemake
 
-snakemake -p -R `cat <(snakemake --lc) <(snakemake --li) <(snakemake --lp)` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --restart-times 1 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
+snakemake -p -R `cat <(snakemake --lc) <(snakemake --li) <(snakemake --lp)` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --restart-times 1 --cluster "sbatch -p {cluster.queue} -c {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
