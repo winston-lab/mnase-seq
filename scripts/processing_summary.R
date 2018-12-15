898ad8248db9189b21c12f13faa388a34d6c6853
James Chuang <chuang.ja@gmail.com>
2018-12-15 15:04:55 -0500

    Make percent loss in library processing summary actually a percentage.

--- scripts/processing_summary.R
+++ scripts/processing_summary.R
@@ -30,36 +30,36 @@ survival_plot = function(df, scalefactor, ylabel){
 main = function(in_table, surv_abs_out, surv_rel_out, loss_out){
     df = read_tsv(in_table) %>%
         mutate(sample=fct_inorder(sample, ordered=TRUE))
-    
+
     nsamples = nrow(df)
-    
-    loss = df %>% gather(step, count, -sample, factor_key=TRUE) %>% 
-        group_by(sample) %>% 
-        mutate(og_count = lag(count)) %>% 
-        filter(step != "raw") %>% 
-        mutate(loss = (og_count-count)/og_count)
-    
+
+    loss = df %>% gather(step, count, -sample, factor_key=TRUE) %>%
+        group_by(sample) %>%
+        mutate(og_count = lag(count)) %>%
+        filter(step != "raw") %>%
+        mutate(loss = (og_count-count)/og_count*100)
+
     #some hacking to get a survival-curve like thing
     #TODO: make the color fill the AUC?
-    survival = df %>% mutate(dummy=raw) %>% 
-        select(sample, dummy, 2:4) %>% 
-        gather(step, count, -sample, factor_key=TRUE) %>% 
+    survival = df %>% mutate(dummy=raw) %>%
+        select(sample, dummy, 2:4) %>%
+        gather(step, count, -sample, factor_key=TRUE) %>%
         mutate_at(vars(step), as.numeric)
-    
+
     surv_abs = survival_plot(survival, scalefactor = 1e6, ylabel = "library size (M reads)") +
         ggtitle("read processing summary",
                 subtitle = "absolute library size")
-    
+
     surv_rel = survival_plot(survival %>% group_by(sample) %>% mutate(count=count/max(count)),
                              scalefactor = .01, ylabel = "% of raw reads") +
         ggtitle("read processing summary", subtitle = "relative to library size")
-        
-    ggsave(surv_abs_out, plot=surv_abs, width=14, height=2+2.5*nsamples, units="cm")
-    ggsave(surv_rel_out, plot=surv_rel, width=14, height=2+2.5*nsamples, units="cm")
-    
+
+    ggsave(surv_abs_out, plot=surv_abs, width=16, height=2+2.5*nsamples, units="cm")
+    ggsave(surv_rel_out, plot=surv_rel, width=16, height=2+2.5*nsamples, units="cm")
+
     loss_plot = ggplot(data = loss, aes(x=step, y=0, fill=loss)) +
         geom_raster() +
-        geom_text(aes(label=round(loss, 2)), size=4) +
+        geom_text(aes(label=round(loss, 1)), size=4) +
         scale_fill_viridis(name="% loss", guide=guide_colorbar(barheight = 10, barwidth=1)) +
         scale_color_viridis(guide=FALSE) +
         scale_x_discrete(labels = c("reads cleaned", "aligned",
@@ -78,8 +78,8 @@ main = function(in_table, surv_abs_out, surv_rel_out, loss_out){
               axis.title.y.right = element_blank(),
               plot.subtitle = element_text(size=12, face="plain"),
               panel.border = element_blank())
-    
-    ggsave(loss_out, plot=loss_plot, width=14, height=2+1.5*nsamples, units="cm")
+
+    ggsave(loss_out, plot=loss_plot, width=16, height=2+1.5*nsamples, units="cm")
 }
 
 main(in_table = snakemake@input[[1]],
